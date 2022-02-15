12527b2bb828507dd6def0e164cb4d20747b3938
Abhishek Singh <abhissm78@gmail.com>
2022-02-15 15:17:06 +0530

    Add project files.

---
+++ StudentDetails/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+
+class Program
+{
+    
+    public static void Main(string[]args)
+    {
+       
+
+
+        Console.WriteLine("Enter thr Student Name");
+        string name = Console.ReadLine();
+        Console.WriteLine("Student ID");
+        int Studentid = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the mark 1");
+        int mark1 = Convert.ToInt32(Console.ReadLine());
+             
+            
+       Console.WriteLine("Enter the mark 2");
+        int mark2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the mark 3");
+        int mark3 = Convert.ToInt32(Console.ReadLine());
+
+
+
+        Student obj = new Student(Studentid, mark1, mark2, mark3, name);
+        obj.SaveFile();
+        
+    }
+}
+
+
+
+
---
+++ StudentDetails/Student.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+class Student
+{
+    int studentid,  mark1, mark2, mark3;
+    string name;
+
+    public Student(int studentid,  int mark1, int mark2, int mark3, string name)
+    {
+        this.studentid = studentid;
+       
+        this.Mark1 = mark1;
+        this.Mark2 = mark2;
+        this.Mark3 = mark3;
+        this.Name = name;
+       
+    }
+
+    public int Studentid { get => studentid; set => studentid = value; }
+  
+    public int Mark1 { get => mark1; set => mark1 = value; }
+    public int Mark2 { get => mark2; set => mark2 = value; }
+    public int Mark3 { get => mark3; set => mark3 = value; }
+    public string Name { get => name; set => name = value; }
+  
+
+    public void SaveFile()
+    {
+        FileStream fs = new FileStream("Student.txt", FileMode.Create, FileAccess.Write);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.WriteLine("Student ID :" + studentid);
+        sw.WriteLine("Name :" + name);
+        sw.WriteLine("Mark 1 :" + mark1);
+        sw.WriteLine("Mark 2 :" + mark2);
+        sw.WriteLine("Mark 3 :" + mark3);
+
+        sw.Close();
+        fs.Close();
+    }
+
+}
+
